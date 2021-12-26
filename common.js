9bd7fd2af8076f5c42c5666e1498ae95fa0be356
Vebnik(Nik) <vebnik111@gmail.com>
2021-12-26 19:36:47 +0300

    Доделал логику подсчёта через eval() Сделал обработку ошибок и заменил <i> для ввода на input

--- common.js
+++ common.js
@@ -2,10 +2,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
     console.log('Common.js loaded ✔')
 
-    let numObj = {
-        num: 0,
-        operator: ''
-    }
 
     function quitApp() {
 
@@ -14,95 +10,42 @@ window.addEventListener('DOMContentLoaded', () => {
             window.close()
         })
     }
-    setTimeout(quitApp, 2000)
+    setTimeout(quitApp, 1000)
+
 
     function logicButton() {
-        let i = 0;
-        let button = document.querySelectorAll('.button-win');
+        let buttonNum = document.querySelectorAll('.button-win');
 
-        while (i < 10) {
-            let textButton = button[i].innerText
-            button[i].addEventListener('click', () => {
+        buttonNum.forEach(el => {
+            el.addEventListener('click', () => {
                 let inputText = document.querySelector('.testInput.quest')
-                inputText.append(textButton)
+                inputText.value += el.innerText
             })
-            ++i
-        }
-
-
-
-    }
-    setTimeout(logicButton, 2000)
-
-    function operatorButton () {
-        let button = document.querySelectorAll('.button-win-op');
-
-        document.querySelector('.clearInput').addEventListener('click', event => {
-            numObj.num = 0
-            document.querySelector('.testInput.answer').innerText = ''
         })
 
-        button.forEach(el => {
-            el.addEventListener('click' , event => {
+        let resultButton = document.querySelector('.result')
 
-                let inputText = document.querySelector('.testInput.quest').innerText
+        resultButton.addEventListener('click', event => {
+            let inputText = document.querySelector('.testInput.quest')
+            let outputText = document.querySelector('.testInput.answer')
 
-                switch (event.target.innerText) {
+            if (inputText.value !== ''){
 
-                    case '+':
-                        if (document.querySelector('.testInput.answer').innerText){
-                            numObj.num =  numObj.num + Number(inputText)
-                            numObj.operator = event.target.innerText
-                            document.querySelector('.testInput.quest').innerText = ''
-                            document.querySelector('.testInput.answer').innerText = numObj.num.toString()
-                            console.log(event.target.innerText, numObj.num)
-                        } else {
-                            numObj.num =  numObj.num + Number(inputText)
-                            numObj.operator = event.target.innerText
-                            document.querySelector('.testInput.quest').innerText = ''
-                            document.querySelector('.testInput.answer').innerText = numObj.num.toString()
-                            console.log(event.target.innerText, numObj.num)
-                        }
-                    return true;
-
-                    case '-':
-                        numObj.num = numObj.num - Number(inputText)
-                        document.querySelector('.testInput.quest').innerText = ''
-                        document.querySelector('.testInput.answer').innerText = numObj.num.toString()
-                        console.log(event.target.innerText, numObj.num)
-                    return true;
-
-                    case `'|'`:
-                        numObj.num = numObj.num / Number(inputText)
-                        document.querySelector('.testInput.quest').innerText = ''
-                        document.querySelector('.testInput.answer').innerText = numObj.num.toString()
-                        console.log(event.target.innerText, numObj.num)
-                    return true;
-
-                    case `X`:
-                        numObj.num = numObj.num * Number(inputText)
-                        document.querySelector('.testInput.quest').innerText = ''
-                        document.querySelector('.testInput.answer').innerText = numObj.num.toString()
-                        console.log(event.target.innerText, numObj.num)
-                    return true;
-
-                    case '=':
-                        if (document.querySelector('.testInput.answer').innerText){
-                            document.querySelector('.testInput.answer').innerText = document.querySelector('.testInput.answer').innerText
-                        }
-
-                    return true;
+                try {
+                    let results = eval(inputText.value)
+                    outputText.innerText = `${results.toFixed(2)}`
+                } catch {
+                    outputText.innerText = 'Error'
                 }
-            })
+
+                inputText.value = ''
+            } else if (inputText.value === '') {
+                outputText.innerText = 'No number!'
+            }
         })
+
+
     }
-    setTimeout(operatorButton, 2000)
-
-    function mainLogic () {
-
-        let inputText = document.getElementsByClassName('testInput')[0].innerText
-
-        inputText.split(`+ X - '/'`)
-    }
+    setTimeout(logicButton, 1000)
 
 });
